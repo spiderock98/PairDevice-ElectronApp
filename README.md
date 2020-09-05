4398e50b293729eb243c1eeb57ebb4ba60a99496
spiderock <spiderock98@gmail.com>
2020-09-05 10:48:56 +0700

    - remove ArduinoBuilder folder in gitignore - [INPOTANT] big re-structure folder >> delete many folder any files - some backup folder is copied to ../Electron.BackupGitignore.090520

--- README.md
+++ README.md
@@ -15,8 +15,52 @@ A basic Electron application needs just these files:
 You can learn more about each of these components within the [Quick Start Guide](https://electronjs.org/docs/tutorial/quick-start).
 
 ## Prerequires
+```
+pip3 install -r requirement.txt
+```
 
-- pip3 install -r requirement.txt
+## Reference to package code to execute file
+```
+electron-packager . mdpreview --platform=win32 --version
+```
+
+## Some tools to upload code
+```
+/////////////////// Windows ///////////////////
+"C:\Program Files (x86)\Arduino\arduino-builder.exe" -compile -warnings=default -verbose -logger=humantags -build-options-file=D:\Desktop\Blink\build.options.json -build-path=D:\Desktop\Blink\build -build-cache=D:\Desktop\Blink\.cache D:\Desktop\Blink\Blink.ino
+
+
+/////////////////// MacOS ///////////////////
+// Failed => Consulting
+/Applications/Arduino.app/Contents/Java/arduino-builder -compile -warnings=default -verbose -logger=humantags -build-options-file=$(pwd)/build-macos.options.json -build-path=$(pwd)/build -build-cache=$(pwd)/.cache $(pwd)/Blink.ino
+
+
+// OK Windows/DATA/Desktop/Blink
+/Applications/Arduino.app/Contents/Java/arduino-builder -compile -logger=machine -hardware /Applications/Arduino.app/Contents/Java/hardware -hardware /Users/spiderock/Library/Arduino15/packages -tools /Applications/Arduino.app/Contents/Java/tools-builder -tools /Applications/Arduino.app/Contents/Java/hardware/tools/avr -tools /Users/spiderock/Library/Arduino15/packages -built-in-libraries /Applications/Arduino.app/Contents/Java/libraries -libraries /Users/spiderock/Documents/Arduino/libraries -fqbn=esp8266:esp8266:nodemcuv2:xtal=80,vt=flash,exception=legacy,ssl=all,eesz=4M2M,led=2,ip=lm2f,dbg=Disabled,lvl=None____,wipe=none,baud=115200 -vid-pid=0000_0000 -ide-version=10811 -build-path $(pwd)/build -warnings=none -build-cache $(pwd)/.cache -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.mklittlefs.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/mklittlefs/2.5.0-4-69bd9e6 -prefs=runtime.tools.mklittlefs-2.5.0-4-69bd9e6.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/mklittlefs/2.5.0-4-69bd9e6 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/2.5.0-4-b40a506 -prefs=runtime.tools.xtensa-lx106-elf-gcc-2.5.0-4-b40a506.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/2.5.0-4-b40a506 -prefs=runtime.tools.mkspiffs.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/mkspiffs/2.5.0-4-b40a506 -prefs=runtime.tools.mkspiffs-2.5.0-4-b40a506.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/mkspiffs/2.5.0-4-b40a506 -prefs=runtime.tools.python3.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/python3/3.7.2-post1 -prefs=runtime.tools.python3-3.7.2-post1.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/python3/3.7.2-post1 -verbose $(pwd)/ArduinoBuilder.ino
+
+
+// upload using upload.py
+python3 /Users/spiderock/Library/Arduino15/packages/esp8266/hardware/esp8266/2.6.3/tools/upload.py --chip esp8266 --port /dev/cu.SLAB_USBtoUART --baud 115200 --before default_reset --after hard_reset write_flash 0x0 $(pwd)/build/Blink.ino.bin
+
+
+// upload using esptool.py
+python3 /Users/spiderock/Library/Arduino15/packages/esp8266/hardware/esp8266/2.6.3/tools/esptool/esptool.py --chip esp8266 --port /dev/cu.SLAB_USBtoUART --baud 115200 --before default_reset --after hard_reset write_flash 0x0 $(pwd)/build/Blink.ino.bin
+
+
+// OK MACOS/Desktop/Blink
+/Applications/Arduino.app/Contents/Java/arduino-builder -compile -logger=machine -hardware /Applications/Arduino.app/Contents/Java/hardware -hardware /Users/spiderock/Library/Arduino15/packages -tools /Applications/Arduino.app/Contents/Java/tools-builder -tools /Applications/Arduino.app/Contents/Java/hardware/tools/avr -tools /Users/spiderock/Library/Arduino15/packages -built-in-libraries /Applications/Arduino.app/Contents/Java/libraries -libraries /Users/spiderock/Documents/Arduino/libraries -fqbn=esp8266:esp8266:nodemcuv2:xtal=80,vt=flash,exception=legacy,ssl=all,eesz=4M2M,led=2,ip=lm2f,dbg=Disabled,lvl=None____,wipe=none,baud=115200 -vid-pid=0000_0000 -ide-version=10811 -build-path $(pwd)/build -warnings=none -build-cache $(pwd)/.cache -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/2.5.0-4-b40a506 -prefs=runtime.tools.xtensa-lx106-elf-gcc-2.5.0-4-b40a506.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/2.5.0-4-b40a506 -prefs=runtime.tools.python3.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/python3/3.7.2-post1 -prefs=runtime.tools.python3-3.7.2-post1.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/python3/3.7.2-post1 -prefs=runtime.tools.mkspiffs.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/mkspiffs/2.5.0-4-b40a506 -prefs=runtime.tools.mkspiffs-2.5.0-4-b40a506.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/mkspiffs/2.5.0-4-b40a506 -prefs=runtime.tools.mklittlefs.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/mklittlefs/2.5.0-4-69bd9e6 -prefs=runtime.tools.mklittlefs-2.5.0-4-69bd9e6.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/mklittlefs/2.5.0-4-69bd9e6 -verbose $(pwd)/ArduinoBuilder.ino
+
+
+cd /Users/spiderock/Desktop/compiler && /Applications/Arduino.app/Contents/Java/arduino-builder -compile -logger=machine -hardware /Applications/Arduino.app/Contents/Java/hardware -hardware /Users/spiderock/Library/Arduino15/packages -tools /Applications/Arduino.app/Contents/Java/tools-builder -tools /Applications/Arduino.app/Contents/Java/hardware/tools/avr -tools /Users/spiderock/Library/Arduino15/packages -built-in-libraries /Applications/Arduino.app/Contents/Java/libraries -libraries /Users/spiderock/Documents/Arduino/libraries -fqbn=esp8266:esp8266:nodemcuv2:xtal=80,vt=flash,exception=legacy,ssl=all,eesz=4M2M,led=2,ip=lm2f,dbg=Disabled,lvl=None____,wipe=none,baud=115200 -vid-pid=0000_0000 -ide-version=10811 -build-path $(pwd)/build -warnings=none -build-cache $(pwd)/.cache -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/2.5.0-4-b40a506 -prefs=runtime.tools.xtensa-lx106-elf-gcc-2.5.0-4-b40a506.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/2.5.0-4-b40a506 -prefs=runtime.tools.python3.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/python3/3.7.2-post1 -prefs=runtime.tools.python3-3.7.2-post1.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/python3/3.7.2-post1 -prefs=runtime.tools.mkspiffs.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/mkspiffs/2.5.0-4-b40a506 -prefs=runtime.tools.mkspiffs-2.5.0-4-b40a506.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/mkspiffs/2.5.0-4-b40a506 -prefs=runtime.tools.mklittlefs.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/mklittlefs/2.5.0-4-69bd9e6 -prefs=runtime.tools.mklittlefs-2.5.0-4-69bd9e6.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/mklittlefs/2.5.0-4-69bd9e6 -verbose $(pwd)/compiler.ino
+
+
+// nodejs quick && command
+cd /Volumes/DATA/Desktop/Blink/ && /Applications/Arduino.app/Contents/Java/arduino-builder -compile -logger=machine -hardware /Applications/Arduino.app/Contents/Java/hardware -hardware /Users/spiderock/Library/Arduino15/packages -tools /Applications/Arduino.app/Contents/Java/tools-builder -tools /Applications/Arduino.app/Contents/Java/hardware/tools/avr -tools /Users/spiderock/Library/Arduino15/packages -built-in-libraries /Applications/Arduino.app/Contents/Java/libraries -libraries /Users/spiderock/Documents/Arduino/libraries -fqbn=esp8266:esp8266:nodemcuv2:xtal=80,vt=flash,exception=legacy,ssl=all,eesz=4M2M,led=2,ip=lm2f,dbg=Disabled,lvl=None____,wipe=none,baud=${baud} -vid-pid=0000_0000 -ide-version=10811 -build-path $(pwd)/build -warnings=none -build-cache $(pwd)/.cache -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.mklittlefs.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/mklittlefs/2.5.0-4-69bd9e6 -prefs=runtime.tools.mklittlefs-2.5.0-4-69bd9e6.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/mklittlefs/2.5.0-4-69bd9e6 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/2.5.0-4-b40a506 -prefs=runtime.tools.xtensa-lx106-elf-gcc-2.5.0-4-b40a506.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/2.5.0-4-b40a506 -prefs=runtime.tools.mkspiffs.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/mkspiffs/2.5.0-4-b40a506 -prefs=runtime.tools.mkspiffs-2.5.0-4-b40a506.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/mkspiffs/2.5.0-4-b40a506 -prefs=runtime.tools.python3.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/python3/3.7.2-post1 -prefs=runtime.tools.python3-3.7.2-post1.path=/Users/spiderock/Library/Arduino15/packages/esp8266/tools/python3/3.7.2-post1 -verbose $(pwd)/Blink.ino && python3 /Users/spiderock/Library/Arduino15/packages/esp8266/hardware/esp8266/2.6.3/tools/esptool/esptool.py --chip esp8266 --port ${port} --baud ${baud} --before default_reset --after hard_reset write_flash 0x0 $(pwd)/build/Blink.ino.bin
+
+
+////// LOCAL BUILDER ///////
+./arduino-builder -compile -logger=machine -hardware ./hardware -hardware ./packages -tools ./tools-builder -tools ./avr -tools ./packages -built-in-libraries ./build_in_libraries -libraries ./user_libraries -fqbn=esp8266:esp8266:nodemcuv2:xtal=80,vt=flash,exception=legacy,ssl=all,eesz=4M2M,led=2,ip=lm2f,dbg=Disabled,lvl=None____,wipe=none,baud=115200 -vid-pid=0000_0000 -ide-version=10811 -build-path $(pwd)/build -warnings=none -build-cache $(pwd)/.cache -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=./01_2.5.0-4-b40a506 -prefs=runtime.tools.xtensa-lx106-elf-gcc-2.5.0-4-b40a506.path=./01_2.5.0-4-b40a506 -prefs=runtime.tools.python3.path=./02_3.7.2-post1 -prefs=runtime.tools.python3-3.7.2-post1.path=./02_3.7.2-post1 -prefs=runtime.tools.mkspiffs.path=./03_2.5.0-4-b40a506 -prefs=runtime.tools.mkspiffs-2.5.0-4-b40a506.path=./03_2.5.0-4-b40a506 -prefs=runtime.tools.mklittlefs.path=./04_2.5.0-4-69bd9e6 -prefs=runtime.tools.mklittlefs-2.5.0-4-69bd9e6.path=./04_2.5.0-4-69bd9e6 -verbose $(pwd)/ArduinoBuilder.ino
+```
 
 ## To Use
 
--- preload.js
+++ preload.js
@@ -116,18 +116,6 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  // fs.readFile(
-  //   "D:\\Desktop\\DATN\\pair-electron-app\\ArduinoBuilder\\ArduinoBuilder.ino",
-  //   { encoding: "utf-8" },
-  //   (err, data) => {
-  //     if (err) {
-  //       console.error(err);
-  //     } else {
-  //       console.log(data);
-  //     }
-  //   }
-  // );
-
   $("#formDevice").on("submit", async (event) => {
     event.preventDefault();
     let ssid =
@@ -137,15 +125,7 @@ window.addEventListener("DOMContentLoaded", () => {
     let name = $("#formDevice").find("input[name='name']").val();
     let baud = $("#formDevice").find("select[name='baud']").val();
     let port = $("#formDevice").find("select[name='port']").val();
-
-    // let esptoolPath = path.join(__dirname, 'esptool', 'esptool.py')
     let uid = await getCurrentUID();
-
-    // let buildFolder = `${path.join(__dirname, 'BuilderLinux')}`
-    // let fileName = 'BuilderLinux.ino'
-
-    /////////////////////COMMENT FROM HERE
-
     let buildFolder = `${path.join(__dirname, "ArduinoBuilder")}`;
     let fileName = "ArduinoBuilder.ino";
     let inoPath = `${path.join(buildFolder, `${fileName}`)}`;
@@ -157,15 +137,6 @@ window.addEventListener("DOMContentLoaded", () => {
 
     console.log("[INFO] Editing ino file ...");
 
-    // console.log("buildFolder:", buildFolder);
-    // console.log("customLibPath:", customLibPath);
-    // console.log("inoPath:", inoPath);
-    // console.log("uid:", uid);
-    // console.log(ssid);
-    // console.log(psk);
-    // console.log(name);
-    // console.log(baud);
-    // console.log(port);
     let espSrcCode = fs.readFileSync(inoPath, { encoding: "utf-8" })
     let oriData = espSrcCode;
     let replaceData = oriData
@@ -178,16 +149,16 @@ window.addEventListener("DOMContentLoaded", () => {
     console.log("[INFO] Building ...");
         switch (process.platform) {
           case "win32":
-            console.log("[INFO] Windows Deteted");
             //!==========================//WINDOWS Buggy//==========================!//
+            console.log("[INFO] Windows Deteted");
             let cmdBuild = spawn(
               `cd ${buildFolder} & arduino-cli.exe core update-index --additional-urls ${addURLs} & arduino-cli.exe compile --additional-urls ${addURLs} --libraries ${customLibPath} --upload --verbose --port ${port} --fqbn=esp8266:esp8266:nodemcuv2:xtal=80,vt=flash,exception=legacy,ssl=all,eesz=4M2M,led=2,ip=lm2f,dbg=Disabled,lvl=None____,wipe=none,baud=${baud} ${buildFolder}`,
               { shell: true },
               (err) => {
                 if (err) {
-                  console.log("[ERROR] ", err);
+                  console.error(err);
                   fs.writeFile(inoPath, oriData, (err) => {
-                    if (err) console.log("[ERROR] ", err);
+                    if (err) console.error(err);
                     console.log("[INFO] Recovered .ino file");
                   });
                   //TODO: log error
@@ -220,7 +191,7 @@ window.addEventListener("DOMContentLoaded", () => {
               );
 
               fs.writeFile(inoPath, oriData, (err) => {
-                if (err) console.log("[ERROR] ", err);
+                if (err) console.error(err);
                 console.log("[INFO] Recovered .ino file");
               });
               // when everything is done successfully
@@ -253,9 +224,9 @@ window.addEventListener("DOMContentLoaded", () => {
               { shell: true, maxBuffer: 1024 * 1024 * 500 },
               (err) => {
                 if (err) {
-                  console.log("[ERROR] ", err);
+                  console.error(err);
                   fs.writeFile(inoPath, oriData, (err) => {
-                    if (err) console.log("[ERROR] ", err);
+                    if (err) console.error(err);
                     console.log("[INFO] Recovered .ino file");
                   });
                   //TODO: log error
@@ -288,7 +259,7 @@ window.addEventListener("DOMContentLoaded", () => {
               );
 
               fs.writeFile(inoPath, oriData, (err) => {
-                if (err) console.log("[ERROR] ", err);
+                if (err) console.error(err);
                 console.log("[INFO] Recovered .ino file");
               });
 
@@ -313,229 +284,11 @@ window.addEventListener("DOMContentLoaded", () => {
                 },
               });
             });
-
             break;
 
           default:
+            console.error("Your Operating System is not supported");
             break;
         }
-    
-    // fs.writeFile(inoPath, replaceData, (err) => {
-    //     if (err) {
-    //       console.error("[ERROR] ", err);
-    //       return;
-    //     }
-        //PUT CODE HERE
-    // });
-
-    // fs.readFile(inoPath, { encoding: "utf-8" }, (err, data) => {
-    //   if (err) {
-    //     console.error("[ERROR] ", err);
-    //     return;
-    //   }
-    //   // let oriData = data;
-    //   // let replaceData = oriData
-    //   //   .replace("taikhoan", ssid)
-    //   //   .replace("matkhau", psk)
-    //   //   .replace("dinhdanh", uid)
-    //   //   .replace("physicalID", name);
-
-      
-    // });
-
-    //!==========================//LINUX a bit laggy//==========================!//
-    // console.log('[INFO] Building ...')
-    // let terminalBuild = spawn(`cd ${buildFolder} && ./arduino-builder -compile -logger=machine -hardware ./hardware -hardware ./packages -tools ./tools-builder -tools ./avr -tools ./packages -built-in-libraries ./libraries-build-in -libraries ./user_libraries -fqbn=esp8266:esp8266:nodemcuv2:xtal=80,vt=flash,exception=legacy,ssl=all,eesz=4M2M,led=2,ip=lm2f,dbg=Disabled,lvl=None____,wipe=none,baud=${baud} -vid-pid=10C4_EA60 -ide-version=10811 -build-path ./build -warnings=none -build-cache ./.cache -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=./2.5.0-4-b40a506-1 -prefs=runtime.tools.xtensa-lx106-elf-gcc-2.5.0-4-b40a506.path=./2.5.0-4-b40a506-1 -prefs=runtime.tools.mklittlefs.path=./2.5.0-4-69bd9e6-2 -prefs=runtime.tools.mklittlefs-2.5.0-4-69bd9e6.path=./2.5.0-4-69bd9e6-2 -prefs=runtime.tools.mkspiffs.path=./2.5.0-4-b40a506-3 -prefs=runtime.tools.mkspiffs-2.5.0-4-b40a506.path=./2.5.0-4-b40a506-3 -prefs=runtime.tools.python3.path=./3.7.2-post1-4 -prefs=runtime.tools.python3-3.7.2-post1.path=./3.7.2-post1-4 -verbose ${inoPath}`, { shell: true, maxBuffer: 1024 * 1024 * 500 }, (err) => {
-    //   if (err) {
-    //     console.log('[ERROR] ', err)
-    //     fs.writeFile(inoPath, oriData, (err) => {
-    //       if (err) console.log('[ERROR] ', err)
-    //       console.log('[INFO] Recovered .ino file')
-    //     })
-    //     //TODO: log error
-    //     return
-    //   }
-    // })
-
-    // let countOut = 0
-    // let countErr = 0
-
-    // // Async Listener
-    // terminalBuild.stdout.on('data', (data) => {
-    //   countOut = countOut + 1
-    //   // console.log(`stdout[${countOut}]: ${data}`)
-    //   $("#progressCompiler").attr('style', `width: ${countOut / 90 * 100}%`)
-    //   // document.getElementById('compilerLog').innerHTML = data
-    // });
-    // terminalBuild.stderr.on('data', (data) => {
-    //   console.error(`stderr: ${data}`)
-    //   countErr = countErr + 1
-    // });
-    // terminalBuild.on('close', (code) => {
-    //   console.log(`[SUCCESS] child process COMPILER exited with code ${code}`)
-    //   console.log(`[INFO] countOut: ${countOut}, countErr: ${countErr}`)
-
-    //   fs.writeFile(inoPath, oriData, (err) => {
-    //     if (err) console.log('[ERROR] ', err)
-    //     console.log('[INFO] Recovered .ino file')
-    //   })
-
-    //   console.log('[INFO] Start Uploading ...');
-    //   let terminalUpload = spawn(`cd ${buildFolder} && python3 ${esptoolPath} --chip esp8266 --port ${port} --baud ${baud} --before default_reset --after hard_reset write_flash 0x0 ${binPath}`, { shell: true }, (err) => {
-    //     if (err) console.log('[ERROR] ', err)
-    //   })
-
-    //   // Async Listener
-    //   terminalUpload.stdout.on('data', (data) => {
-    //     console.log(`stdout: ${data}`);
-    //   });
-    //   terminalUpload.stderr.on('data', (data) => {
-    //     console.error(`stderr: ${data}`);
-    //   });
-    //   terminalUpload.on('close', (code) => {
-    //     console.log(`[SUCCESS] child process UPLOAD exited with code ${code}`);
-
-    //     // Everything is done
-    //     $.ajax({
-    //       url: '/home/newDevices',
-    //       method: 'POST',
-    //       data: {
-    //         name: $('#formDevice').find("input[name='name']").val(),
-    //         loc: $('#formDevice').find("input[name='loc']").val(),
-    //         //TODO: ssid is <input> or <select>
-    //         ssid: $('#formDevice').find("input[name='ssid']").val(),
-    //         psk: $('#formDevice').find("input[name='psk']").val(),
-    //         baud: $('#formDevice').find("select[name='baud']").val(),
-    //         port: $('#formDevice').find("select[name='port']").val(),
-    //       },
-    //       success: () => {
-    //         location.href = '/'
-    //       }
-    //     })
-    //   });
-    // });
-
-    //!==========================//WINDOWS Buggy//==========================!//
-    // let buildFolder = `${path.join(__dirname, "arduino-cli_0.9.0_Windows_64bit")}`
-    // let fileName = 'BuilderWindows.ino'
-    // let folderName = 'BuilderWindows'
-
-    // let inoPath = `${path.join(buildFolder, folderName, `${fileName}`)}`
-
-    // console.log('[INFO] Editing .ino info');
-    // fs.readFile(inoPath, { encoding: 'utf-8' }, (err, data) => {
-    //   if (err) { console.log('[INFO] ', err); return; }
-    //   let oriData = data
-    //   let replaceData = oriData.replace('taikhoan', ssid).replace('matkhau', psk).replace('dinhdanh', uid)
-    //   fs.writeFile(inoPath, replaceData, err => {
-    // if (err) { console.log('[INFO] ', err); return; }
-
-    //     let terminalBuild = spawn(`cd ${buildFolder} && ./arduino-cli core update-index --additional-urls ${addURLs} && ./arduino-cli compile --additional-urls ${addURLs} --libraries ${customLibPath} --upload --verbose --port ${port} --fqbn=esp8266:esp8266:nodemcuv2:xtal=80,vt=flash,exception=legacy,ssl=all,eesz=4M2M,led=2,ip=lm2f,dbg=Disabled,lvl=None____,wipe=none,baud=${baud} ${buildFolder}`, { shell: true, maxBuffer: 1024 * 1024 * 500 }, (err) => {
-    //     console.log('[INFO] Building ...')
-    //     let terminalBuild = spawn(`cd ${buildFolder} && ./arduino-cli.exe compile -p ${port} --fqbn=esp8266:esp8266:nodemcuv2:xtal=80,vt=flash,exception=legacy,ssl=all,eesz=4M2M,led=2,ip=lm2f,dbg=Disabled,lvl=None____,wipe=none ${fileName}`, { maxBuffer: 1024 * 1024 * 500 }, (err) => {
-    //       if (err) {
-    //         console.log('[ERROR] ', err)
-    //         fs.writeFile(inoPath, oriData, (err) => {
-    //           if (err) console.log('[ERROR] ', err)
-    //           console.log('[INFO] Recovered .ino file')
-    //         })
-    //         //TODO: log error
-    //         return
-    //       }
-    //     })
-
-    //     let countOut = 0
-    //     let countErr = 0
-
-    //     // Async Listener
-    //     terminalBuild.stdout.on('data', (data) => {
-    //       countOut = countOut + 1
-    //       console.log(`stdout[${countOut}]: ${data}`)
-    //       $("#progressCompiler").attr('style', `width: ${countOut / 90 * 100}%`)
-    //       // document.getElementById('compilerLog').innerHTML = data
-    //     });
-    //     terminalBuild.stderr.on('data', (data) => {
-    //       console.error(`stderr: ${data}`)
-    //       countErr = countErr + 1
-    //     });
-    //     terminalBuild.on('close', (code) => {
-    //       console.log(`[SUCCESS] child process COMPILER exited with code ${code}`)
-    //       console.log(`[INFO] countOut: ${countOut}, countErr: ${countErr}`)
-
-    //       fs.writeFile(inoPath, oriData, (err) => {
-    //         if (err) console.log('[ERROR] ', err)
-    //         console.log('[INFO] Recovered .ino file')
-    //       })
-
-    //       console.log('[INFO] Start Uploading ...');
-    //       let terminalUpload = exec(`cd ${buildFolder} && ./arduino-cli.exe upload -p ${port} --fqbn=esp8266:esp8266:nodemcuv2:xtal=80,vt=flash,exception=legacy,ssl=all,eesz=4M2M,led=2,ip=lm2f,dbg=Disabled,lvl=None____,wipe=none,baud=${baud}`, { windowsHide: true, maxBuffer: 1024 * 1024 * 500 }, (err) => {
-    //         if (err) console.log('[ERROR] ', err)
-    //       })
-
-    //       // Async Listener
-    //       terminalUpload.stdout.on('data', (data) => {
-    //         console.log(`stdout: ${data}`);
-    //       });
-    //       terminalUpload.stderr.on('data', (data) => {
-    //         console.error(`stderr: ${data}`);
-    //       });
-    //       terminalUpload.on('close', (code) => {
-    //         // TODO: child process COMPILER exited with code 126 // .ino file error
-    //         console.log(`[SUCCESS] child process UPLOAD exited with code ${code}`);
-
-    //         // when everything is done successfully
-    //         $.ajax({
-    //           url: '/home/newDevices',
-    //           method: 'POST',
-    //           data: {
-    //             name: $('#formDevice').find("input[name='name']").val(),
-    //             loc: $('#formDevice').find("input[name='loc']").val(),
-    //             //TODO: ssid is <input> or <select>
-    //             ssid: $('#formDevice').find("input[name='ssid']").val() || $('#formDevice').find("select[name='ssid']").val(),
-    //             psk: $('#formDevice').find("input[name='psk']").val(),
-    //             baud: $('#formDevice').find("select[name='baud']").val(),
-    //             port: $('#formDevice').find("select[name='port']").val(),
-    //           },
-    //           success: () => { location.href = '/' }
-    //         })
-    //       })
-    //     })
-    //   })
-    // })
-
-    //!==========================//LINUX too laggy//==========================!//
-    // var ls = spawn(`cd /media/spiderock/DATA/Desktop/pair-electron-app/BuilderLinux && ./arduino-builder -compile -logger=machine -hardware ./hardware -hardware ./packages -tools ./tools-builder -tools ./avr -tools ./packages -built-in-libraries ./libraries-build-in -libraries ./user_libraries -fqbn=esp8266:esp8266:nodemcuv2:xtal=80,vt=flash,exception=legacy,ssl=all,eesz=4M2M,led=2,ip=lm2f,dbg=Disabled,lvl=None____,wipe=none,baud=115200 -vid-pid=10C4_EA60 -ide-version=10811 -build-path ./build -warnings=none -build-cache ./.cache -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=./2.5.0-4-b40a506-1 -prefs=runtime.tools.xtensa-lx106-elf-gcc-2.5.0-4-b40a506.path=./2.5.0-4-b40a506-1 -prefs=runtime.tools.mklittlefs.path=./2.5.0-4-69bd9e6-2 -prefs=runtime.tools.mklittlefs-2.5.0-4-69bd9e6.path=./2.5.0-4-69bd9e6-2 -prefs=runtime.tools.mkspiffs.path=./2.5.0-4-b40a506-3 -prefs=runtime.tools.mkspiffs-2.5.0-4-b40a506.path=./2.5.0-4-b40a506-3 -prefs=runtime.tools.python3.path=./3.7.2-post1-4 -prefs=runtime.tools.python3-3.7.2-post1.path=./3.7.2-post1-4 -verbose ./BuilderLinux.ino`, { shell: true, maxBuffer: 1024 * 1024 * 500 }, (err) => {
-    //   if (err) console.log('[ERROR] ', err)
-    //   else {
-    //     console.log('[INFO] Done Compile and Uploading');
-    //   }
-    // })
-    // ls.stdout.on('data', (data) => {
-    //   console.log(`stdout: ${data}`);
-    // });
-    // ls.stderr.on('data', (data) => {
-    //   console.error(`stderr: ${data}`);
-    // });
-    // ls.on('close', (code) => {
-    //   console.log(`child process exited with code ${code}`);
-    // });
-
-    //!======================//BACKUP MACOS just uploading//======================!//
-
-    // exec(`python3 ${esptoolPath} --chip esp8266 --port ${port} --baud ${baud} --before default_reset --after hard_reset write_flash 0x0 ArduinoBuilder/build/ArduinoBuilder.ino.bin`, err => {
-    //   if (err) console.log('[INFO] ', err)
-    //   else {
-    //     console.log('[INFO] Done Uploading');
-    //   }
-    // })
-
-    // var jsonStr = JSON.stringify({
-    //   "PORT": port,
-    //   "BAUD": baud
-    // })
-    // fs.writeFile('config/config.json', jsonStr, (err) => {
-    //   if (err) console.log(err)
-    //   else { console.log('[INFO] Done update config.json') }
-    // })
   });
 });
